775cf18785ef2a9ef6d1716d93fff63567c1bdb1
Anusri A <anusria304@gmail.com>
2025-07-07 11:56:21 +0530

    Correct route format

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import express from "express";
+import movieRoutes from "./routes/movies.route.js";
 
 const app = express();
 const PORT = 7068;
 
 app.get("/", (req,res)=>{
-    res.json({msg: "Hello interviwees"});
+    res.json({msg: "Hello interviewees"});
 });
+
+app.use('/movies', movieRoutes);
 
 app.listen(PORT, () => {
     console.log(`The server is running at http://localhost:${PORT}`)
