ed6aa1377e28f46e71156cf812c068231583da5f
Anusri A <anusria304@gmail.com>
2025-07-08 14:49:13 +0530

    Add model

--- controllers/movies.controllers.js
+++ controllers/movies.controllers.js
@@ -1,11 +1,28 @@
+import Movie from "../models/movies.model.js";
+
 export const MovieIndex = (req,res) => {
     res.send("Gett all movie list");
 };
 
-export const MovieCreate = (req,res) => {
+export const MovieCreate = async(req,res) => {
     //res.send("Create a movie");
-    console.log(req.body);
-    return res.json(req.body);
+    //console.log(req.body);
+
+    const newMovie = new Movie({  //called as constructor
+        title: req.body.title,
+        desc: req.body.desc,
+    });
+
+    try {
+        const movie = await newMovie.save();
+        return res.status(201).json(movie); //201 - create  
+    } catch (error) {
+        return res.status(400).json({ message : error.message});
+    }
+
+   
+
+    //return res.json(req.body);
 };
 
 export const MovieUpdate = (req,res) =>{
---
+++ models/movies.model.js
@@ -0,0 +1,10 @@
+import { Schema, model } from "mongoose" ;
+
+const schema = new Schema({
+    title: String,
+    desc : String,
+})
+
+const Movie = model("Movie", schema);
+
+export default Movie;
